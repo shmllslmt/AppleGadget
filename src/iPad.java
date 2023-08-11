1bc83ba7303f06eb60fe257caf3195e46258b6af
shmll <shmllslmt@gmail.com>
2023-08-11 15:47:55 +0800

    Initial commit

---
+++ src/iPad.java
@@ -0,0 +1,46 @@
+public class iPhone implements AppleDevice {
+    private String deviceName;
+    private String model;
+    private int storage;
+
+    public iPhone() {
+        this("iPhone", "SE", 64);
+    }
+    public iPhone(String deviceName, String model, int storage) {
+        this.deviceName = deviceName;
+        this.model = model;
+        this.storage = storage;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    @Override
+    public void connectToWiFi() {
+        System.out.println(deviceName + " " + model +" is connecting to WiFi...");
+    }
+
+    @Override
+    public void sendData(String data) {
+        System.out.println(deviceName + " " + model + " sends " + data);
+    }
+
+    @Override
+    public void receiveData(String data) {
+        System.out.println(" to " + deviceName + " " + model);
+    }
+
+    @Override
+    public String toString() {
+        return deviceName + " " + model + " with storage capacity of " + storage + "GB";
+    }
+}
